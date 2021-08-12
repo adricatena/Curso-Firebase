ed11b2e234fe2d413b2a659661450379997f2205
Adriano <57508409+adricatena@users.noreply.github.com>
2021-08-12 12:15:46 -0300

    rearmado de proyecto

--- react-firebase/src/App.js
+++ react-firebase/src/App.js
@@ -1,13 +1,25 @@
-import "./App.css";
-import { useGlobalContext } from "./context";
-import Home from "./components/Home/Home";
-import { db, auth, storage } from "./firebase";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  console.log(db, auth, storage);
-  return <main>
-    <Home />
-  </main>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
